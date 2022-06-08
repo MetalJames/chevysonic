a8e4f1f9a993e83adc2c2d4f5cca128e13428a8e
Volodymyr <volodyaruzhak@gmail.com>
2022-06-08 14:57:14 +0300

    working on background form multiple devices add some default stylings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
 const App = () => {
-  return (
-    <div>Chevrolet Sonic 2018 LT Hatchback</div>
-  )
+    return (
+        <div>Chevrolet Sonic 2018 LT Hatchback</div>
+    )
 }
 
 export default App;
