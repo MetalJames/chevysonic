30af39236cf5ff63e059fcbee530a3f1831ee9f5
Volodymyr <volodyaruzhak@gmail.com>
2022-06-09 12:33:14 +0300

    add general padding and margin Created the main page

---
+++ src/containers/chevy_sonic/ChevySonic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChevySonic = () => {
+    return (
+        <div className='chevy__home section__padding' id='home'>
+            ChevySonic
+        </div>
+    )
+}
+
+export default ChevySonic
