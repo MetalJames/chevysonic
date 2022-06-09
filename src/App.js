75f4a7cad72f2e094aad85f4f81a3f33644d8587
Volodymyr <volodyaruzhak@gmail.com>
2022-06-09 18:34:32 +0300

    worked on chevy about section feature component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import ChevySonic from './containers/chevy_sonic/ChevySonic';
 
 const App = () => {
     return (
         <>
             <Navbar />
+            <ChevySonic />
         </>
     )
 }
--- src/containers/chevy_sonic/ChevySonic.jsx
+++ src/containers/chevy_sonic/ChevySonic.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './chevy_sonic.css';
+import Feature from '../../components/feature/Feature';
+import logo from '../../assets/new.jpg'
 
 const ChevySonic = () => {
     return (
-        <div className='chevy__home section__padding' id='home'>
-            ChevySonic
+        <div className='gpt3__whatgpt3 section__margin' id='home'>
+            <div className='gpt3__whatgpt3-heading'>
+                <h1>Chevrolet Sonic 2018 LT Hatchback</h1>
+            </div>
+            <div className='gpt3__whatgpt3-container'>
+                <Feature title='NEW STYLE AND NEW FEATURES' text='New sonic 2018 lt hatchback made in the way you will like it. Many new features like: heated seats, sunroof, spoiler at the back, toned windows, make this car look really awesome. A small car was never looked better, and you will be surprised by how fast and maneuverable this car can be.' img={logo} />
+                <Feature title='NEW STYLE AND NEW FEATURES' text='New sonic 2018 lt hatchback made in the way you will like it. Many new features like: heated seats, sunroof, spoiler at the back, toned windows, make this car look really awesome. A small car was never looked better, and you will be surprised by how fast and maneuverable this car can be.' img={logo} />
+                <Feature title='NEW STYLE AND NEW FEATURES' text='New sonic 2018 lt hatchback made in the way you will like it. Many new features like: heated seats, sunroof, spoiler at the back, toned windows, make this car look really awesome. A small car was never looked better, and you will be surprised by how fast and maneuverable this car can be.' img={logo} />
+            </div>
         </div>
     )
 }
