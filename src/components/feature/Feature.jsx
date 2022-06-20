67b40b6ce629b45a03c91fc535ef90fe9f9a748d
Volodymyr <volodyaruzhak@gmail.com>
2022-06-20 18:46:11 +0300

    have done some work on chevysonic page - it is hard

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,12 +3,12 @@ import './feature.css';
 
 const Feature = ({ title, text, img }) => {
     return (
-        <div className='gpt3__features-container__feature'>
-            <div className='gpt3__features-container__feature-title .section__margin'><div />
+        <div className='chevysonic__features-container__feature'>
+            <div className='chevysonic__features-container__feature-title .section__margin'><div />
                 <h1>{title}</h1><div />
             </div>
             
-            <div className='gpt3__features-container_feature-text'>
+            <div className='chevysonic__features-container_feature-text'>
                 <img src={img} alt="img" />
                 <p>{text}</p>
             </div>
--- src/containers/chevy_sonic/ChevySonic.jsx
+++ src/containers/chevy_sonic/ChevySonic.jsx
@@ -5,11 +5,11 @@ import logo from '../../assets/new.jpg'
 
 const ChevySonic = () => {
     return (
-        <div className='gpt3__whatgpt3 section__margin' id='home'>
-            <div className='gpt3__whatgpt3-heading'>
+        <div className='chevysonic section__margin' id='home'>
+            <div className='chevysonic-heading'>
                 <h1>Chevrolet Sonic 2018 LT Hatchback</h1>
             </div>
-            <div className='gpt3__whatgpt3-container'>
+            <div className='chevysonic-container'>
                 <Feature title='NEW STYLE AND NEW FEATURES' text='New sonic 2018 lt hatchback made in the way you will like it. Many new features like: heated seats, sunroof, spoiler at the back, toned windows, make this car look really awesome. A small car was never looked better, and you will be surprised by how fast and maneuverable this car can be.' img={logo} />
                 <Feature title='NEW STYLE AND NEW FEATURES' text='New sonic 2018 lt hatchback made in the way you will like it. Many new features like: heated seats, sunroof, spoiler at the back, toned windows, make this car look really awesome. A small car was never looked better, and you will be surprised by how fast and maneuverable this car can be.' img={logo} />
                 <Feature title='NEW STYLE AND NEW FEATURES' text='New sonic 2018 lt hatchback made in the way you will like it. Many new features like: heated seats, sunroof, spoiler at the back, toned windows, make this car look really awesome. A small car was never looked better, and you will be surprised by how fast and maneuverable this car can be.' img={logo} />
