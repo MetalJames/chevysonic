6fcd46e2b05b03005dea8294d07eec9a3b8b27fc
Volodymyr <volodyaruzhak@gmail.com>
2022-06-22 16:26:58 +0300

    link some images

---
+++ src/components/dimentions/Dimentions.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import chevysonic_dimentions from '../../assets/chevysonic_dimentions.svg';
+import './dimentions.css';
+
+const Dimentions = () => {
+    return (
+        <div className='chevysonic__dimentions-container__feature'>
+            <div className='chevysonic__dimentions-container__feature-title .section__margin'><div />&nbsp;&nbsp;&nbsp;
+                <h1>DIMENTIONS</h1>&nbsp;&nbsp;&nbsp;<div />
+            </div>
+            <img src={chevysonic_dimentions} alt='chevy sonic dimentions' />
+        </div>
+    )
+}
+
+export default Dimentions
